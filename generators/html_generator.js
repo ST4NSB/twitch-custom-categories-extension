f103596dc175cb4cb9318eae351e5ea6796f17c6
ST4NSB <barbulescuadrian10@gmail.com>
2023-03-06 19:29:02 +0200

    added html generators + 2 buttons for categories

---
+++ generators/html_generator.js
@@ -0,0 +1,74 @@
+function addCategoryButton() {
+  const element = document.querySelector(".gdKXDc");
+  element.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="Layout-sc-1xcs6mc-0 hjmDKV tooltip" id="addCustomCategory" title="Add a custom Category">
+    <div class="Layout-sc-1xcs6mc-0 onsite-notifications">
+        <div class="Layout-sc-1xcs6mc-0 hIWHob onsite-notifications-toast-manager"
+            data-test-selector="onsite-notifications-toast-manager">
+        </div>
+        <div class="Layout-sc-1xcs6mc-0 bZVrjx">
+            <div class="Layout-sc-1xcs6mc-0 crbrgc">
+                <div style="display: inherit;" data-test-selector="toggle-balloon-wrapper__mouse-enter-detector">
+                    <div class="InjectLayout-sc-1i43xsx-0 dVOhMf">
+                        <button class="ScCoreButton-sc-ocjdkq-0 ibtYyW ScButtonIcon-sc-9yap0r-0 iqxxop"
+                            aria-label="Open Notifications" aria-describedby="71pItpr26nPuC7mqdyBUN61xDZtH2sDj">
+                            <div class="ButtonIconFigure-sc-1emm8lf-0 bxQRSJ">
+                                <div class="ScIconLayout-sc-1q25cff-0 cMWGQu">
+                                    <div class="ScAspectRatio-sc-18km980-1 hTTohL tw-aspect">
+                                        <div class="ScAspectSpacer-sc-18km980-0 kiiGFY">
+                                        </div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                            <path fill="white" 
+                                                d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z" />
+                                            </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`
+  );
+}
+
+function deleteCategorryButton() {
+  const element = document.querySelector(".gdKXDc");
+  element.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="Layout-sc-1xcs6mc-0 hjmDKV tooltip" id="deleteCustomCategory" title="Delete a custom Category">
+    <div class="Layout-sc-1xcs6mc-0 onsite-notifications">
+        <div class="Layout-sc-1xcs6mc-0 hIWHob onsite-notifications-toast-manager"
+            data-test-selector="onsite-notifications-toast-manager">
+        </div>
+        <div class="Layout-sc-1xcs6mc-0 bZVrjx">
+            <div class="Layout-sc-1xcs6mc-0 crbrgc">
+                <div style="display: inherit;" data-test-selector="toggle-balloon-wrapper__mouse-enter-detector">
+                    <div class="InjectLayout-sc-1i43xsx-0 dVOhMf">
+                        <button class="ScCoreButton-sc-ocjdkq-0 ibtYyW ScButtonIcon-sc-9yap0r-0 iqxxop"
+                            aria-label="Open Notifications" aria-describedby="71pItpr26nPuC7mqdyBUN61xDZtH2sDj">
+                            <div class="ButtonIconFigure-sc-1emm8lf-0 bxQRSJ">
+                                <div class="ScIconLayout-sc-1q25cff-0 cMWGQu">
+                                    <div class="ScAspectRatio-sc-18km980-1 hTTohL tw-aspect">
+                                        <div class="ScAspectSpacer-sc-18km980-0 kiiGFY">
+                                        </div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                            <path
+                                                fill="white"
+                                                d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+                                            </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`
+  );
+}
--- main_page.js
+++ main_page.js
@@ -1,26 +1,63 @@
 async function main() {
-  await deleteCategory("test");
-  // await deleteChannel("xqc", "test");
-  // await deleteChannel("forsen", "test");
-
-  // await deleteCategory("test2");
-  // await deleteChannel("xqc", "test2");
-
-  // await addCategory("test");
-  // await addChannelToCategory("xqc", "test");
-  // await addChannelToCategory("forsen", "test");
-
-  // await addCategory("test2");
-  // await addChannelToCategory("xqc", "test2");
-
-  //const token = await getOAUTH2Token();
-  //const liveUsers = await getLiveChannels(["forsen", "AustinShow"], token);
-
-  //console.log("live users: ", liveUsers);
-  //console.log("all channels: ", await getAllChannels());
-
   try {
-    //throw new Error("Cannot divide by zero.");
+    deleteCategorryButton();
+    addCategoryButton();
+    const addCategoryButtonId = document.getElementById("addCustomCategory");
+    const deleteCategoryButtonId = document.getElementById(
+      "deleteCustomCategory"
+    );
+
+    addCategoryButtonId.addEventListener("click", async function () {
+      const category = prompt(
+        "Please enter a custom category name:",
+        ""
+      ).trim();
+      if (!category || category === "") {
+        throw new Error("Invalid category name!");
+      }
+
+      await addCategory(category);
+      console.log(
+        "Twitch Custom Categories - storage categories:",
+        await getCategories()
+      );
+    });
+
+    deleteCategoryButtonId.addEventListener("click", async function () {
+      const allCategories = await getCategories();
+      if (allCategories.length === 0) {
+        alert("There are NO existing categories!");
+        return;
+      }
+
+      let categoriesDictionary = {};
+      let promptMessage =
+        "Delete a category by typing the numbers splitted by comma ',': \n";
+
+      for (let i = 0; i < allCategories.length; i++) {
+        promptMessage += `${i}: ${allCategories[i]} \n`;
+        categoriesDictionary[i] = allCategories[i];
+      }
+
+      const categories = prompt(promptMessage, "")
+        .trim()
+        .split(",")
+        .map(Number);
+      if (!categories || categories === "") {
+        throw new Error("Invalid categories!");
+      }
+
+      for (let i = 0; i < categories.length; i++) {
+        let number = categories[i];
+        let category = categoriesDictionary[number];
+        await deleteCategory(category);
+      }
+
+      console.log(
+        "Twitch Custom Categories - storage categories:",
+        await getCategories()
+      );
+    });
   } catch (err) {
     const msg = `Twitch Custom Categories - main_page.js - ${err}`;
     console.log(msg);
